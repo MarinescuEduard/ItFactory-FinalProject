36e7c0af7d34fb6773e8c7089dc8194714d6b10b
MarinescuEduard <marinescu.eduard98@gmail.com>
2024-03-23 00:41:35 +0200

    Secont commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Proiect Java
+# header H1
+## header H2
+### header H3
+#### header H4
+##### header H5
+###### header H6
--- src/test/java/com/itfactory/EduardMarinescuJvjs5JavaProjectApplicationTests.java
+++ src/test/java/com/itfactory/EduardMarinescuJvjs5JavaProjectApplicationTests.java
@@ -5,9 +5,8 @@ import com.itfactory.model.Person;
 import com.itfactory.service.PersonService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,24 +14,22 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 class EduardMarinescuJvjs5JavaProjectApplicationTests {
 
-    @Mock
-    private PersonService personService;
-    @InjectMocks
+    @Autowired
     private PersonController personController;
+
+    @Autowired
+    private PersonService personService;
 
     @Test
     void testAdaugaPersoana() {
-
-        //Create a mock person
         Person person = new Person();
         person.setIdPerson(1);
         person.setFirstName("Eduard");
@@ -40,30 +37,20 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
         person.setEmailPerson("eduard.marinescu98@gmail.com");
         person.setAgePerson(26);
 
-        //Mock the behaviour of the method .createPerson and return the mocked person
-        when(personService.createPerson(person)).thenReturn(person);
         ResponseEntity<Person> responseEntity = personController.createPerson(person);
 
-        //Check if the response status is OK and the created person is the mocked person
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(person, responseEntity.getBody());
-
     }
-
 
     @Test
     void testAduToatePersoanele() {
-
-        //Create a mock list
         List<Person> personList = new ArrayList<>();
         personList.add(new Person(3, "Rosy", "Rosy", "bobobobob@yahoo.com", 26));
         personList.add(new Person(1, "Eduard", "Marinescu", "eduard.marinescu98@gmail.com", 26));
 
-        //Mock the behaviour of .aduToatePersoanele method
-        when(personService.aduToatePersoanele()).thenReturn(personList);
         ResponseEntity<List<Person>> responseEntity = personController.aduToatePersoanele();
 
-        //Check if the response status is OK and the created list includes the mocked persons
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(personList, responseEntity.getBody());
     }
@@ -72,7 +59,6 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
     void testFindPersonByID() {
         int personId = 1;
 
-        //Create a mock person
         Person person = new Person();
         person.setIdPerson(1);
         person.setFirstName("Eduard");
@@ -80,12 +66,8 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
         person.setEmailPerson("eduard.marinescu98@gmail.com");
         person.setAgePerson(26);
 
-
-        //Mock the behaviour of the .findById method
-        when(personService.findById(personId)).thenReturn(person);
         ResponseEntity<Person> responseEntity = personController.findPersonById(personId);
 
-        //Check if the response status is OK and the returned person is the mocked person
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(person, responseEntity.getBody());
     }
@@ -94,22 +76,16 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
     void testDeleteById() {
         int personId = 1;
 
-        //Mock the behaviour of the .deleteById method
-        doNothing().when(personService).deleteById(personId);
         ResponseEntity<Void> responseEntity = personController.deleteById(personId);
 
-        //Verify if .deleteById was called with the correct argument
-        verify(personService).deleteById(personId);
-        //Verify the response status is NO CONTENT
         assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
     }
 
     @Test
-    void testModifyLastNameById(){
-        int personId=1;
+    void testModifyLastNameById() {
+        int personId = 1;
         String newPersonLastName = "NewLastName";
 
-        //Create a mock person
         Person person = new Person();
         person.setIdPerson(1);
         person.setFirstName("Eduard");
@@ -117,26 +93,19 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
         person.setEmailPerson("eduard.marinescu98@gmail.com");
         person.setAgePerson(26);
 
-        //Mock the behaviour of .findById and .createPerson methods
-        when(personService.findById(personId)).thenReturn(person);
-        when(personService.createPerson(any(Person.class))).thenReturn(person);
-        ResponseEntity<Person> responseEntity = personController.modifyNameById(personId,newPersonLastName);
+        Optional<Person> optionalPerson = Optional.of(person);
 
-        //Verify that findById was called with correct argument and if .createPerson was called with the mocked person.
-        verify(personService).findById(personId);
-        verify(personService).createPerson(person);
+        ResponseEntity<Person> responseEntity = personController.modifyNameById(personId, newPersonLastName);
 
-        //Verify the response status and if the new last name was modified
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(newPersonLastName, responseEntity.getBody().getLastName());
     }
 
     @Test
-    void testModifyEmailById(){
+    void testModifyEmailById() {
         int personId = 1;
         String newEmailAddress = "NewEmailAddress@gmail.com";
 
-        //Create a mock person
         Person person = new Person();
         person.setIdPerson(1);
         person.setFirstName("Eduard");
@@ -144,16 +113,10 @@ class EduardMarinescuJvjs5JavaProjectApplicationTests {
         person.setEmailPerson("eduard.marinescu98@gmail.com");
         person.setAgePerson(26);
 
-        //Mock the behaviour of .findById and .createPerson methods
-        when(personService.findById(personId)).thenReturn(person);
-        when(personService.createPerson(any(Person.class))).thenReturn(person);
+        Optional<Person> optionalPerson = Optional.of(person);
+
         ResponseEntity<Person> responseEntity = personController.modifyEmailById(personId, newEmailAddress);
 
-        //Verify that findById was called with correct argument and if .createPerson was called with the mocked person.
-        verify(personService).findById(personId);
-        verify(personService).createPerson(person);
-
-        //Verify the response status and if the new email was modified
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(newEmailAddress, responseEntity.getBody().getEmailPerson());
     }
