f841ac72723ca70ab8e54e0ca0a1569ed9af3b0b
Eduard Marinescu <156937215+MarinescuEduard@users.noreply.github.com>
2024-04-06 13:38:41 +0300

    Add files via upload

--- finalProject.sql
+++ finalProject.sql
@@ -16,6 +16,77 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: person; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.person (
+    id_person integer NOT NULL,
+    age_person integer,
+    email_person character varying(255),
+    first_name character varying(255),
+    last_name character varying(255)
+);
+
+
+ALTER TABLE public.person OWNER TO postgres;
+
+--
+-- Name: person_id_person_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.person_id_person_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.person_id_person_seq OWNER TO postgres;
+
+--
+-- Name: person_id_person_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.person_id_person_seq OWNED BY public.person.id_person;
+
+
+--
+-- Name: person id_person; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person ALTER COLUMN id_person SET DEFAULT nextval('public.person_id_person_seq'::regclass);
+
+
+--
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.person (id_person, age_person, email_person, first_name, last_name) FROM stdin;
+\.
+
+
+--
+-- Name: person_id_person_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.person_id_person_seq', 1, false);
+
+
+--
+-- Name: person person_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT person_pkey PRIMARY KEY (id_person);
+
+
 --
 -- PostgreSQL database dump complete
 --
